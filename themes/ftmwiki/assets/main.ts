cfb512730bbfb7d31444938f4ad7f310c5cc70ed
Wester001 <176673076+Wester001@users.noreply.github.com>
2026-04-12 15:16:41 +0000

    feat: 添加站内搜索（Fuse.js）

--- themes/ftmwiki/assets/main.ts
+++ themes/ftmwiki/assets/main.ts
@@ -1,3 +1,7 @@
 import './functions/header'
 import './functions/anchor'
 import './functions/content'
+import { initSearch } from './functions/search'
+import { ready } from './functions/_utils'
+
+ready.then(initSearch)
