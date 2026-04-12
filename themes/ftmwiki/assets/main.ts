7e9d5200f72908d8dd1ad0e879839b109b046588
Wester001 <176673076+Wester001@users.noreply.github.com>
2026-04-12 15:17:20 +0000

    feat: 添加夜间模式

--- themes/ftmwiki/assets/main.ts
+++ themes/ftmwiki/assets/main.ts
@@ -2,6 +2,8 @@ import './functions/header'
 import './functions/anchor'
 import './functions/content'
 import { initSearch } from './functions/search'
+import { initTheme } from './functions/theme'
 import { ready } from './functions/_utils'
 
 ready.then(initSearch)
+ready.then(initTheme)
